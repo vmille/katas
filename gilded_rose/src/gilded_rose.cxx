e66266ff282bf31f741d4be1082513883a73250f
Vivien MILLE <mille.vivien@proton.me>
2024-08-26 23:04:37 +0200

    merging condition for default behavior

--- gilded_rose/src/gilded_rose.cxx
+++ gilded_rose/src/gilded_rose.cxx
@@ -44,13 +44,9 @@ namespace vm {
         if (item.quality > 0) {
           item.quality = item.quality - 1;
         }
-
         item.sell_in = item.sell_in - 1;
-
-        if (item.sell_in < 0) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
+        if (item.sell_in < 0 && item.quality > 0) {
+          item.quality = item.quality - 1;
         }
       }
     }
