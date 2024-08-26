41cd7bf1fc149c72e3080948e014e0018ee0828e
Vivien MILLE <mille.vivien@proton.me>
2024-08-26 22:43:28 +0200

    merging conditions

--- gilded_rose/src/gilded_rose.cxx
+++ gilded_rose/src/gilded_rose.cxx
@@ -11,27 +11,20 @@ namespace vm {
   void gilded_rose::update_quality () {
     for (int i = 0; i < items.size(); i++) {
       if (items[i].name != aged_brie && items[i].name != backstage_passes) {
-        if (items[i].quality > 0) {
-          if (items[i].name != sulfuras) {
-            items[i].quality = items[i].quality - 1;
-          }
+        if (items[i].quality > 0 && items[i].name != sulfuras) {
+          items[i].quality = items[i].quality - 1;
         }
       }
       else {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1;
-
           if (items[i].name == backstage_passes) {
-            if (items[i].sell_in < 11) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-              }
+            if (items[i].sell_in < 11 && items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
             }
 
-            if (items[i].sell_in < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-              }
+            if (items[i].sell_in < 6 && items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
             }
           }
         }
@@ -44,20 +37,16 @@ namespace vm {
       if (items[i].sell_in < 0) {
         if (items[i].name != aged_brie) {
           if (items[i].name != backstage_passes) {
-            if (items[i].quality > 0) {
-              if (items[i].name != sulfuras) {
-                items[i].quality = items[i].quality - 1;
-              }
+            if (items[i].quality > 0 && items[i].name != sulfuras) {
+              items[i].quality = items[i].quality - 1;
             }
           }
           else {
             items[i].quality = items[i].quality - items[i].quality;
           }
         }
-        else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
-          }
+        else if (items[i].quality < 50) {
+          items[i].quality = items[i].quality + 1;
         }
       }
     }
