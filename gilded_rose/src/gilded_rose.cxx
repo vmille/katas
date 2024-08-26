4552d61149d6dcd2b2742dff10641a977817cd3a
Vivien MILLE <mille.vivien@proton.me>
2024-08-26 22:48:17 +0200

    for-each loop

--- gilded_rose/src/gilded_rose.cxx
+++ gilded_rose/src/gilded_rose.cxx
@@ -9,44 +9,44 @@ namespace vm {
   gilded_rose::gilded_rose (std::vector<item>& items) : items(items) {}
 
   void gilded_rose::update_quality () {
-    for (int i = 0; i < items.size(); i++) {
-      if (items[i].name == sulfuras) {
+    for (auto& item : items) {
+      if (item.name == sulfuras) {
         continue;
       }
-      if (items[i].name != aged_brie && items[i].name != backstage_passes) {
-        if (items[i].quality > 0 && items[i].name != sulfuras) {
-          items[i].quality = items[i].quality - 1;
+      if (item.name != aged_brie && item.name != backstage_passes) {
+        if (item.quality > 0 && item.name != sulfuras) {
+          item.quality = item.quality - 1;
         }
       }
       else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-          if (items[i].name == backstage_passes) {
-            if (items[i].sell_in < 11) {
-              items[i].quality = items[i].quality + 1;
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.name == backstage_passes) {
+            if (item.sell_in < 11) {
+              item.quality = item.quality + 1;
             }
-            if (items[i].sell_in < 6) {
-              items[i].quality = items[i].quality + 1;
+            if (item.sell_in < 6) {
+              item.quality = item.quality + 1;
             }
           }
         }
       }
 
-      items[i].sell_in = items[i].sell_in - 1;
+      item.sell_in = item.sell_in - 1;
 
-      if (items[i].sell_in < 0) {
-        if (items[i].name != aged_brie) {
-          if (items[i].name != backstage_passes) {
-            if (items[i].quality > 0 && items[i].name != sulfuras) {
-              items[i].quality = items[i].quality - 1;
+      if (item.sell_in < 0) {
+        if (item.name != aged_brie) {
+          if (item.name != backstage_passes) {
+            if (item.quality > 0 && item.name != sulfuras) {
+              item.quality = item.quality - 1;
             }
           }
           else {
-            items[i].quality = items[i].quality - items[i].quality;
+            item.quality = item.quality - item.quality;
           }
         }
-        else if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
+        else if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       }
     }
